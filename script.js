1a84cb2e36aa6ca0313c8b7826493d575bc1f918
HabLubis <166669557+HabLubis@users.noreply.github.com>
2026-05-11 05:38:10 +0700

    hero

--- script.js
+++ script.js
@@ -2,13 +2,16 @@ document.addEventListener('DOMContentLoaded', function() {
     const hamburger = document.getElementById('hamburger');
     const navMenu = document.getElementById('navMenu');
     
-    hamburger.addEventListener('click', function() {
-        navMenu.classList.toggle('active');
-    });
-    
-    document.querySelectorAll('.nav-link').forEach(link => {
-        link.addEventListener('click', () => {
-            navMenu.classList.remove('active');
+    if (hamburger && navMenu) {
+        hamburger.addEventListener('click', function() {
+            navMenu.classList.toggle('active');
         });
-    });
+    
+        document.querySelectorAll('.nav-link').forEach(link => {
+            link.addEventListener('click', () => {
+                navMenu.classList.remove('active');
+            });
+        });
+    }
+
 });
