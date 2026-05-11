0b1c912e731dea127c4ff8961453be278591d264
HabLubis <166669557+HabLubis@users.noreply.github.com>
2026-05-11 12:14:27 +0700

    update js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-document.addEventListener('DOMContentLoaded', ()=> {
+document.addEventListener('DOMContentLoaded', function() {
     const hamburger = document.getElementById('hamburger');
     const navMenu = document.getElementById('navMenu');
     
     if (hamburger && navMenu) {
-        hamburger.addEventListener('click', ()=> {
+        hamburger.addEventListener('click', function() {
             navMenu.classList.toggle('active');
             hamburger.querySelector('i').classList.toggle('fa-bars');
             hamburger.querySelector('i').classList.toggle('fa-times');
